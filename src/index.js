44c5fde78d4107b1b591a262819cbde996c5f223
Antonijo <antonijodev@gmail.com>
2019-05-22 15:12:15 +0100

    Added Todo component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import todoApp from './reducers';
 import './index.css';
+
+const store = createstore(todoApp);
 
 ReactDOM.render(
   <App />,
