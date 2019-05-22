4e4689449bfa24df3d3984e4c5fdadc5e7678b88
Antonijo <antonijodev@gmail.com>
2019-05-22 16:21:45 +0100

    Added data to index.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
+import App from './components/App';
 import './index.css';
 
-const store = createstore(todoApp);
+const store = createStore(todoApp);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
